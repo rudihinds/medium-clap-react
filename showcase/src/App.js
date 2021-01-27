20a47cd62c86f5bdb824acddd9b322d8b5a79495
Rudi Hinds <rudihinds@gmail.com>
2021-01-27 12:14:25 +0000

    initial commit

--- showcase/src/App.js
+++ showcase/src/App.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
-import { hot } from 'react-hot-loader'
-import styled from 'styled-components'
-import { useMediaQuery } from 'react-responsive'
+import React, { useState } from "react";
+import { hot } from "react-hot-loader";
+import styled from "styled-components";
+import { useMediaQuery } from "react-responsive";
+import MediumClapComponent from "./MediumClapComponent";
 
-import Sidebar from './Sidebar'
-import Body from './Body'
-import { SCREEN_SIZES, media } from './content/StyledContent'
+import Sidebar from "./Sidebar";
+import Body from "./Body";
+import { SCREEN_SIZES, media } from "./content/StyledContent";
 
 const StyledContainer = styled.div`
   display: grid;
@@ -16,24 +17,37 @@ const StyledContainer = styled.div`
       'sidebar body'
       1fr /24% 1fr;
   `}
-`
+`;
 
 const App = () => {
-  const isMediumOrLarger = useMediaQuery({ minWidth: SCREEN_SIZES.md })
+  const isMediumOrLarger = useMediaQuery({ minWidth: SCREEN_SIZES.md });
 
   // toggle sidebar display
-  const [showSidebar, setShowSidebar] = useState(false)
-  const isSidebarShown = isMediumOrLarger || (!isMediumOrLarger && showSidebar)
+  const [showSidebar, setShowSidebar] = useState(false);
+  const isSidebarShown = isMediumOrLarger || (!isMediumOrLarger && showSidebar);
 
   return (
-    <StyledContainer>
-      {isSidebarShown && <Sidebar setShowSidebar={setShowSidebar} />}
-      <Body
-        setShowSidebar={setShowSidebar}
-        isMediumOrLarger={isMediumOrLarger}
-      />
-    </StyledContainer>
-  )
-}
+    <div
+      style={{
+        display: "block",
+        position: "absolute",
+        top: "48%",
+        left: "48%",
+      }}
+    >
+      <MediumClapComponent />
+    </div>
+  );
+};
 
-export default hot(module)(App)
+export default hot(module)(App);
+
+// return (
+//   <StyledContainer>
+//     {isSidebarShown && <Sidebar setShowSidebar={setShowSidebar} />}
+//     <Body
+//       setShowSidebar={setShowSidebar}
+//       isMediumOrLarger={isMediumOrLarger}
+//     />
+//   </StyledContainer>
+// )
